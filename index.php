bd3daeb099ecb3533c7f5679fc18e337018c206d
Diceon <Diceons@gmail.com>
2019-03-03 01:20:15 +0200

    Fixed htaccess, adjusted index

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
+
 require_once './inc/connection.php';
 
-$db = new connection('localhost', 'API-1', 'root', '');
-
+$db = new connection('localhost', 'API-1', 'API-1', 'API-1');
+print_r($_GET);
 if (filter_input(INPUT_SERVER, "REQUEST_METHOD") == "GET") {
-    //echo "GET";
     echo json_encode($db->query("SELECT * FROM chat"));
+
+    if ($_GET["url"] == "auth") {
+        echo "Authorization request";
+    } else if ($_GET["url"] == "bla") {
+        echo "blabla";
+    }
+
     http_response_code(200);
 } else if (filter_input(INPUT_SERVER, "REQUEST_METHOD") == "POST") {
-    echo "POST";
+    echo $_SERVER["REQUEST_METHOD"];
+    
+    if ($_GET["url"] == "auth") {
+        $postBody = file_get_contents("php://input");
+        echo $postBody . "test";
+    }
 } else {
     http_response_code(405);
 }
